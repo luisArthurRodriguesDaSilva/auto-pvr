4ee11d83e8e52c3cc7d4ffa762c3a955d5504502
luis arthur <luisarthurlards03@gmail.com>
2023-03-17 17:48:12 -0300

    organization

--- req.py
+++
@@ -1,8 +0,0 @@
-import requests
-bible =(requests.get('https://raw.githubusercontent.com/thiagobodruk/bible/master/json/pt_nvi.json').json())
-for book  in bible :
-  if (book["abbrev"] == "pv"):
-    proverbius = book
-    break
-
-print(proverbius['chapters'][0])
