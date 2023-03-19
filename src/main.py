b2bfea102a23f41fa04ad627bc93036ce0a43d35
luis arthur <luisarthurlards03@gmail.com>
2023-03-19 20:22:04 -0300

    prepare to testDeploy

--- src/main.py
+++ src/main.py
@@ -6,22 +6,23 @@ import os
 import time
 
 isProduction = lambda : os.getenv('producao') == 'true' 
-
-for versicleCordinates in popularVersicles:
-  try:
-    [book,capterNumber,versicleNumber] = versicleCordinates
-    print(versicleCordinates)
-    versicleText = (getVersicle(book,capterNumber,versicleNumber))
-
-    rawImagePath = imgf.raffleImage('fotos maneras')
-    editedImage = imgf.getEditedImage(rawImagePath)
-    imgWithText = imgf.putTextOnImage(editedImage,versicleText,versicleCordinates)
-    finalImagePath = imgf.saveImage(imgWithText,versicleCordinates)["newPath"]
-    
-    postIt(finalImagePath) if isProduction() else imageToMyDm(finalImagePath)
+while 1 :
+  for versicleCordinates in popularVersicles:
+    try:
+      [book,capterNumber,versicleNumber] = versicleCordinates
+      print(versicleCordinates)
+      versicleText = (getVersicle(book,capterNumber,versicleNumber))
   
-  except Exception as e:
-    notifyByDm(e)
-  print('to sleepando')
-  time.sleep(int(os.getenv('interval')))
-  print('sleepei')
+      rawImagePath = imgf.raffleImage('fotos maneras')
+      editedImage = imgf.getEditedImage(rawImagePath)
+      imgWithText = imgf.putTextOnImage(editedImage,versicleText,versicleCordinates)
+      finalImagePath = imgf.saveImage(imgWithText,versicleCordinates)["newPath"]
+      
+      postIt(finalImagePath) if isProduction() else imageToMyDm(finalImagePath)
+    
+    except Exception as e:
+      notifyByDm(e)
+    print('to sleepando')
+    time.sleep(int(os.getenv('interval')))
+    print('sleepei')
+  
