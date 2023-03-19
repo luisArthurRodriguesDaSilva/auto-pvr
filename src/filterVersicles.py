7dd49e367738f78daff77056053253a486472bf8
luis arthur <luisarthurlards03@gmail.com>
2023-03-19 20:37:06 -0300

    deploy test

--- src/filterVersicles.py
+++ src/filterVersicles.py
@@ -2,10 +2,10 @@ from versicles import popularVersicles
 from bibleApi import getVersicle
 
 aprovados = []
-for v in popularVersicles[50:60]:
+for v in popularVersicles[20:50]:
   [book,capterNumber,versicleNumber] = v
   versicleText = (getVersicle(book,capterNumber,versicleNumber))
-  print(versicleText)
+  print('\n\n',versicleText,'\n')
   aprovados.append(v) if input('aprovado?') == 's' else None
 
 with open("src/aprovados.py", 'w') as file:
